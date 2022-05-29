4c0d4bc9a0d8f9cd3f598911c8d5705f696f8a99
RemoteCity <857782993@qq.com>
2022-05-30 00:25:09 +0800

    problem 8. part

---
+++ src/main/java/com/whk/problems01_50/problems08/stringToTnteger_atoi/Main.java
@@ -0,0 +1,11 @@
+package com.whk.problems01_50.problems08.stringToTnteger_atoi;
+
+
+import com.whk.problems01_50.problems08.stringToTnteger_atoi.a.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        System.out.println(solution.myAtoi("010"));
+    }
+}
