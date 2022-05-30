7f1dc510194592be6e013abdd09513a6c33c6643
RemoteCity <857782993@qq.com>
2022-05-31 00:49:30 +0800

    problem 9. main

---
+++ src/main/java/com/whk/problems01_50/problems09/palindromeNumber/Main.java
@@ -0,0 +1,12 @@
+package com.whk.problems01_50.problems09.palindromeNumber;
+
+
+import com.whk.problems01_50.problems09.palindromeNumber.a.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        boolean palindrome = solution.isPalindrome(100);
+        System.out.println(palindrome);
+    }
+}
